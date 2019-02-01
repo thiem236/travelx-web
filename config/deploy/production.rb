91d9a4c780e6c09eee14b63d56940a7afa950889
Tung Pham <kyo.shenshie@gmail.com>
2019-02-01 11:39:19 +0700

    Add ruby version and production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@ set :ssh_options,     {
     auth_methods: %w(publickey)
 }
 
-set :puma_threads, [1, 6]
-set :puma_workers, 1
+set :puma_threads, [4, 16]
+set :puma_workers, 2
 set :stage, :production
 set :puma_workers, ENV['TRAVELX_PRODUCTION_CPU_CORE_NUMBER']
