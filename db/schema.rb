2cf8d74f37d5efb4cb97a3e3c437c4922c2f074d
lamle154 <=>
2019-02-28 19:07:05 +0700

    rever schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190228114846) do
+ActiveRecord::Schema.define(version: 20190209151737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -216,7 +216,7 @@ ActiveRecord::Schema.define(version: 20190228114846) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "from_date"
+    t.date "form_date"
     t.date "to_date"
   end
 
