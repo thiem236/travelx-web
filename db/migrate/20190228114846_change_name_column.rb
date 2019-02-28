4dd2316d77f2941273212b89c11c333095f01d17
lamle154 <=>
2019-02-28 19:10:13 +0700

    change migration

--- db/migrate/20190228114846_change_name_column.rb
+++ db/migrate/20190228114846_change_name_column.rb
@@ -1,5 +1,5 @@
 class ChangeNameColumn < ActiveRecord::Migration[5.1]
   def change
-    rename_column :todo_lists, :form_data, :from_date
+    rename_column :todo_lists, :form_date, :from_date
   end
 end
