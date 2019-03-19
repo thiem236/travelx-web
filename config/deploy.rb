8bcb725b8741a158f104bad86a290a72fa0991fa
Lê Thanh Lâm <lamle154@gmail.com>
2019-03-19 16:55:54 +0700

    revert deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,14 +81,9 @@ namespace :deploy do
   #     execute "sudo service nginx restart"
   #   end
   # end
-  task :reindex_search do
-    on roles(:all) do
-      execute "bundle exec rake searchkick:reindex CLASS=User "
-    end
-  end
 
   before 'deploy:check:linked_files', :copy
-  after  :finishing, :reindex_search
+  # after  :finishing, :nginx
   after  :finishing, :cleanup
   after  :finishing, :restart
 end
