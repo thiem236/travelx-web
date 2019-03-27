d805deca45f6995a69da7aa521329e1948350873
Lê Thanh Lâm <lamle154@gmail.com>
2019-03-27 15:39:05 +0700

    add routes well know

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   end
   
   def apple_app_site
-    association_json = File.read(Rails.public_path + "apple_app_site_association")
+    association_json = File.read(Rails.public_path + "apple-app-site-association")
     render :json => association_json, :content_type => "application/json"
   end
 
